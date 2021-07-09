d5ba7a063115a6c7330f0fa5ff155e63c41c6ee6
ZiffyClumper <70002194+ziffyclumper@users.noreply.github.com>
2021-07-08 19:30:01 -0700

    beans

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -40,7 +40,7 @@ class MainMenuState extends MusicBeatState
 	public static var nightly:String = "";
 
 	public static var kadeEngineVer:String = "1.5.1" + nightly;
-	public static var gameVer:String = "1.0";
+	public static var gameVer:String = "1.1";
 
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
